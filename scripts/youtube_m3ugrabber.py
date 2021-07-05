0c8d4e2ad80eeec93558e651a5f65149922d1f18
IPTVtuner <68898576+IPTVtuner@users.noreply.github.com>
2021-07-06 00:09:45 +0100

    Update youtube_m3ugrabber.py

--- scripts/youtube_m3ugrabber.py
+++ scripts/youtube_m3ugrabber.py
@@ -56,7 +56,7 @@ with open('../youtube_channel_info.txt') as f:
             grp_title = line[1].strip().title()
             tvg_logo = line[2].strip()
             tvg_id = line[3].strip()
-            print(f'#EXTINF:-1 group-title="{grp_title}" tvg-logo="../logos/{ch_name}.png" tvg-id="{tvg_id}", {ch_name}')
+            print(f'#EXTINF:-1 group-title="{grp_title}" tvg-logo="{ch_name}.png" tvg-id="{tvg_id}", {ch_name}')
         else:
             grab(line)
             
